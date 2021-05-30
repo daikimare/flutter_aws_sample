24695e48b433229e3aedab9e85f5a1812954f576
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-05-31 04:51:14 +0900

    create login form

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override 
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Photo Gallery App',
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+
+      home: Navigator(
+        pages: [MaterialPage(child: LoginPage())],
+        onPopPage: (route, result) => route.didPop(result),
+      ),
+    );
+  }
+}
