19928a1436690de58ee99875c4332e7f2a9acb50
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-06-17 01:38:35 +0900

    set up aws

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './amplifyconfiguration.dart';
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter_aws_sample/auth_service.dart';
 import 'package:flutter_aws_sample/camera_flow.dart';
 import 'package:flutter_aws_sample/login_page.dart';
@@ -10,71 +12,79 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  @override 
+  @override
   State<StatefulWidget> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  /* ↓ This sentence was rejected new version for flutter.
+       There for it is delete */
+  // final _amplify = Amplify();
   final _authService = AuthService();
 
   @override
   void initState() {
     super.initState();
+    _configureAmplify();
     _authService.showLogin();
   }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Photo Gallery App',
       theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: StreamBuilder<AuthState>(
+          stream: _authService.authStateController.stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Navigator(
+                pages: [
+                  // show login page
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.login)
+                    MaterialPage(
+                        child: LoginPage(
+                            didProvideCredentials:
+                                _authService.loginWithCredentials,
+                            shouldShowSignUp: _authService.showSignUp)),
 
-      home: StreamBuilder<AuthState> (
-        stream: _authService.authStateController.stream,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Navigator(
-              pages: [
-                // show login page
-                if (snapshot.data.authFlowStatus == AuthFlowStatus.login)
-                MaterialPage(
-                  child: LoginPage(
-                    didProvideCredentials: _authService.loginWithCredentials,
-                    shouldShowSignUp: _authService.showSignUp
-                  )
-                ),
+                  // show sign up page
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.singup)
+                    MaterialPage(
+                        child: SignUpPage(
+                            didProvideCredentials:
+                                _authService.signUpWithCredentials,
+                            shouldShowLogin: _authService.showLogin)),
 
-                // show sign up page
-                if (snapshot.data.authFlowStatus == AuthFlowStatus.singup)
-                MaterialPage(
-                  child: SignUpPage(
-                    didProvideCredentials: _authService.signUpWithCredentials,
-                    shouldShowLogin: _authService.showLogin
-                  )
-                ),
-                
-                if (snapshot.data.authFlowStatus == AuthFlowStatus.vertification)
-                MaterialPage(
-                  child: VerificationPage(
-                    didProvideVerificationCode: _authService.verifyCode,
-                  )
-                ),
+                  if (snapshot.data.authFlowStatus ==
+                      AuthFlowStatus.vertification)
+                    MaterialPage(
+                        child: VerificationPage(
+                      didProvideVerificationCode: _authService.verifyCode,
+                    )),
 
-                if (snapshot.data.authFlowStatus == AuthFlowStatus.session)
-                MaterialPage(
-                  child: CameraFlow(shouldLogOut: _authService.logOut)
-                )
-              ],
-              onPopPage: (route, result) => route.didPop(result),
-            );
-          } else {
-            return Container(
-              alignment: Alignment.center,
-              child: CircularProgressIndicator(),
-            );
-          }
-        }
-      ),
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.session)
+                    MaterialPage(
+                        child: CameraFlow(shouldLogOut: _authService.logOut))
+                ],
+                onPopPage: (route, result) => route.didPop(result),
+              );
+            } else {
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
     );
+  }
+
+  void _configureAmplify() async {
+    try {
+      await Amplify.configure(amplifyconfig);
+      print('Successfully configure Amplify 🎉 ');
+    } catch (e) {
+      print('Could not configure Amplify ☠️ ');
+    }
   }
 }
